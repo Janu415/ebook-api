4e590b4f82c55ded8284423f84bf241c96f1c6f1
Janu415 <Janugirindraa@gmail.com>
2022-10-13 10:05:17 +0700

    coba

--- ebook/routes/web.php
+++ ebook/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Return_;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('coba', function () {
+    return "Halo" ;
 });
